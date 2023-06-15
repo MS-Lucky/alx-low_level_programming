0da1a3f9cffee623dab9eb4e7c29bd6c79452a75
milky <mylucky2001@gmail.com>
2023-06-16 01:45:26 +0300

    Hello,Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
