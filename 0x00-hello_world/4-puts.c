02bcd32168f57ed1d9b359b1b9787ada958d6338
milky <mylucky2001@gmail.com>
2023-06-16 01:58:40 +0300

    Hello,Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main: Entry point
+ * main:main function 
  *
- * Return: 0 success
+ * Return: 0 
  */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts("Programming is like building a multilingual puzzle.");
+	return 0;
 }
