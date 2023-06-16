a7f6a62b14b18ed474cd4aa73309b93a2e3fb035
milky <mylucky2001@gmail.com>
2023-06-17 02:37:28 +0300

    Print lower and upper case followed by newline

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * maint - Entry of the program
+ * main - Entry of the program
  * Discription : Print out lowwwer case and uppder case follws by newline
  * Return:0
  */
 int main(void)
 {
 	char alphabet, uppercase;
+	uppercase = toupper (alphabet);
 	
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 	putchar(alphabet);
 	}
-	uppercase = toupper(alphabet);
 	putchar(uppercase);
 	{
 	putchar('\n');
