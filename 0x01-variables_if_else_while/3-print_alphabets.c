8792cad6dd7cd4424b270c1537c1cc454c108cfe
milky <mylucky2001@gmail.com>
2023-06-17 02:12:14 +0300

    Print lower and upper case followed by newline

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * maint - Entry of the program
+ * Discription : Print out lowwwer case and uppder case follws by newline
+ * Return:0
+ */
+int main(void)
+{
+	char alphabet;
+	char uppercase;
+	
+	uppercase = toupper(alphabet);
+
+	for (alphabet = 'a'; alphabet= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+		putchar(uppercase);
+		{
+			putchar('\n');
+			return (0);
+		}
+}
