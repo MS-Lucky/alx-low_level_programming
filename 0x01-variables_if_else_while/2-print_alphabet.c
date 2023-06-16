db3cb7f023529c919519bd3447dea3441daddd8d
milky <mylucky2001@gmail.com>
2023-06-17 01:46:29 +0300

    print alphabets in lower case using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entery of main program
+ * Description : lists down small alpahbets letters 
+ * Return:0
+ */
+int main()
+{
+	char alphabet;
+
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+	{
+	putchar(alphabet);
+	putchar ('\n');
+	}
+	return (0);
+}
