9a9d885f5b01c41d9be85410d49c07868e553df1
milky <mylucky2001@gmail.com>
2023-06-17 02:32:14 +0300

    Print lower and upper case followed by newline

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 	putchar(alphabet);
-	
+	}
 	uppercase = toupper(alphabet);
 	putchar(uppercase);
-	}
+	{
 	putchar('\n');
-	
+	}
 	return (0);
 }
