9dddf738a75b12be051be986f3ce9aef656c1c0c
milky <mylucky2001@gmail.com>
2023-06-17 22:59:15 +0300

    prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,14 @@
  * Description: prints all single digit numbers of base 10 starting from 0
  * Return: 0
  */
-int main()
+int main(void)
 {
 	int i;
+	
 	for (i = 0; i < 10; i++)
 	{
-	putchar(i);
+	putchar(i + '0');
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
