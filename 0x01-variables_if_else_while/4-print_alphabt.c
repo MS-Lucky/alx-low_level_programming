d00acc5e49eebf9309cbfe419863fd6953324fce
milky <mylucky2001@gmail.com>
2023-06-17 04:29:49 +0300

    Print lettercs except specifid character

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,11 @@
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		if ( alpha != 'q' || alpha != 'e')
+		if ( alpha != 'q' && alpha != 'e')
 		{
 			putchar(alpha);
 		}
 		putchar('\n');
-
 		return (0);
 	}
 }
