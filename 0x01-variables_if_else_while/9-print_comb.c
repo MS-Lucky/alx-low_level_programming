3d6c9629c1fa7aa11f45cf59b75754edbc0cfa7c
milky <mylucky2001@gmail.com>
2023-06-18 04:50:46 -0700

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,13 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i=0; i <=9; i++)
 	{
-		for (j=0; j <=9; j++)
-		{
-		putchar (i + '0');
-		putchar (j + '0');
-
-		if (i < 9 || j < 9)
-		{
-		putchar(',');
-		}
-		}
+	putchar (i + '0');
 	}
-
+	putchar(',');
 	putchar('\n');
 
 	return (0);
