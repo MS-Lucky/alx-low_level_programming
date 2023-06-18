64b08f7f29b7db279749ee343dea1b7f419b0c6d
Milky <mylucky2001@gmail.com>
2023-06-18 16:29:41 +0300

    How to print all single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-ii * main - Entry point of the program
- * Description : the program will print all possible combaination of single digits
+ * main - Entry point of the program
+ * Description : the program will print all single digits
  * Return:0
  */
 int main(void)
 {
 	int i;
 
-	for (i= 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 	putchar (i + '0');
 	if (i != 9)
@@ -18,7 +18,6 @@ int main(void)
 	putchar(' ');
 	}
 		}
-	
 	putchar('\n');
 	return (0);
 }
