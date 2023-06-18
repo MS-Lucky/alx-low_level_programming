e1d62f434de7cd8bfb1524d46e5ccc79a1f717c8
Milky <mylucky2001@gmail.com>
2023-06-18 16:20:00 +0300

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,16 @@ int main(void)
 {
 	int i;
 
-	for (i= 0; i <= 8; i++)
+	for (i= 0; i <= 9; i++)
 	{
 	putchar (i + '0');
-	putchar(',');
+	if (i != 9)
+	{
+	putchar (',');
+	putchar(' ');
 	}
-	putchar('9');
+		}
+	
 	putchar('\n');
 	return (0);
 }
