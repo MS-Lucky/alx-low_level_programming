c5d4b62a34f3290acc8a97aa04a770bcd3a8392e
Milky <mylucky2001@gmail.com>
2023-06-18 17:09:48 +0300

    How to print all two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Entry point of the program
- * Description : the program will print all possible combaination 
+ * Description : the program will print all possible combaination
  * Return:0
  */
 int main(void)
 {
 	int i, j;
 
-	for (i=0; i <=9; i++)
-        {
-	for (j=i+1; j <=9; j++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar (i + '0');
-                putchar (j + '0');
+	for (j = i + 1; j <= 9; j++)
+	{
+	putchar (i + '0');
+	putchar (j + '0');
 
 	if (!(i == 8 && j == 9))
 	{
