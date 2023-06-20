31177459678ac8369ff94d9b69779115647296dc
MS-Lucky <mylucky2001@gmail.com>
2023-06-20 22:03:49 +0300

    rints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,14 @@
  * Description : Prints 10 times the alphabets
  * Return:0
  */
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	while (i <= 10)
 	{
 		char j;
+
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(i);
