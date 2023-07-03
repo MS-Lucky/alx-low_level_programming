fafa1b154795bfd2831cc8b722312247d4f61be5
milky <mylucky2001@gmail.com>
2023-07-03 13:34:38 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+/**
+ * _strspn - writes the character c to stdout
+ * @s: The character to print
+ * @accept: char
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j] == '\0')
+			return (i);
+	}
+	return (i);
+}
+
