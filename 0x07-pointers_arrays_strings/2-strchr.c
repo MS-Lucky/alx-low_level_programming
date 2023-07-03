f6eaed8f9f6b0176bbedeac1d84cf7cb7ef55e13
milky <mylucky2001@gmail.com>
2023-07-03 13:28:58 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * _strchr - writes the character c to stdout
+ * @s: The character to print
+ * @c: char
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
+
