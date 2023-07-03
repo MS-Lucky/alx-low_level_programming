6f6e4badbc369e7b02e0c8d06e041f1c86a5a507
milky <mylucky2001@gmail.com>
2023-07-03 13:14:01 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,16 @@
 
 /**
  * _memset - writes the character c to stdout
- * @c: The character to print
- *
+ * @s: The character to print
+ * @b: char
+ * @n: char
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
