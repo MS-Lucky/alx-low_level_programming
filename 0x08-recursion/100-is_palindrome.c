2159f1b59af1e3572893052c68ddd31072837c97
milky <mylucky2001@gmail.com>
2023-07-05 14:14:06 -0700

    advanced task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if s is a palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int j = strlen(s) - 1;
+
+	while (i < j)
+	{
+		if (s[i] != s[j])
+			return (0);
+		i++;
+		j--;
+	}
+	return (1);
+}
