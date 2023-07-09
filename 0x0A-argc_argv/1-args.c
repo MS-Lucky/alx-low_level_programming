97546a6d8258eeb89ae553331299a97f831d7b12
milky <mylucky2001@gmail.com>
2023-07-09 03:23:06 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- start of the progam
+ * Description: print it self
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
