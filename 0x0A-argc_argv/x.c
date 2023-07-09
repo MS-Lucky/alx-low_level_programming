e04bc893a2cce6f668402015a12314af0f5c63e1
milky <mylucky2001@gmail.com>
2023-07-09 09:56:05 -0700

    4-add.c

---
+++ 0x0A-argc_argv/x.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i, j, num;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[i]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
