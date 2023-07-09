6cf6b2c40ff3d80c539d600f429cf46d51fed1a7
milky <mylucky2001@gmail.com>
2023-07-09 06:14:57 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: argumnet vector
+ *
+ * Description: main program
+ *
+ * Return: 0 always
+ */
 int main(int argc, char *argv[])
 {
 	(void) argc;
