00e40592838597d7dd0a39c5ebccecc3a1c254e2
milky <mylucky2001@gmail.com>
2023-07-10 22:02:13 +0300

    1-strdup.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
-void simple_print_buffer(char *buffer, unsigned int size);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+#endif
