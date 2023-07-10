44fe3547cb66c9b9514cf06831d52edd7a4913d7
milky <mylucky2001@gmail.com>
2023-07-10 12:31:12 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup -main function
+ * @str: char
+ * Return:0
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+	new_str = malloc(strlen(str) + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+	strcpy(new_str, str);
+	return (new_str);
+}
