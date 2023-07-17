bd11936a6a71432ffe8b55d900c79bd96aea3c36
milky <mylucky2001@gmail.com>
2023-07-17 06:30:20 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog -new dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: dog object having 3 props
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
