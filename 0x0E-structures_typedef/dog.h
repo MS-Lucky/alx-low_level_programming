558436e9d51129c2db97a340df50403c59a2c4a1
milky <mylucky2001@gmail.com>
2023-07-17 22:34:32 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,19 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * typedef struct -define new object
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: define new data type
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void str_cpy(char *src, char *dest);
 #endif
+
