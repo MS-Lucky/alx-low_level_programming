52f63dd4e1ab5a6f7e7a160ee0793a78ffb65ef9
milky <mylucky2001@gmail.com>
2023-07-19 01:28:47 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,14 +4,13 @@
 /**
  * free_dog - free memory allocated
  * @d: memory pointer for dog object
- *
- * Description: free memory for dog object
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free((*d).name);
-	free((*d).owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
