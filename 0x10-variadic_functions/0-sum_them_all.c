f5b081f14e6e1924582116337951dce7d6eb43f1
milky <mylucky2001@gmail.com>
2023-07-21 09:52:46 -0700

    vardiac late sumbition

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
- * sum_them_all - varidia function
- * @n : number of arguments
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if(n == 0)
-	{
-		return (0);
-	}
-	va_list arg;
-	unsigned i;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	int sum = 0;
-	
-	va_start(arg, n);
-	for(i=0; i<n; i++)
-		sum+=va_arg(arg,int);
-	va_end(arg);
-	return(sum);
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
 }
