cfc01ab7edf166d534f81c21cfe12dd4ded5d0a9
root <mylucky2001@gmail.com>
2023-07-27 13:09:39 -0700

    Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,69 @@
+#include <stdio.h>
+#include <string.h>
 #include "lists.h"
-/**
- * add_node_end - add node to end of list
- * @head: the head pointer
- * @str: default str
- *
- * Return: address of new element
- */
-list_t *add_node_end(list_t **head, const char *str)
-{
-	list_t *Node, *p;
-	unsigned int i;
+#include <stdlib.h>
 
-	Node = malloc(sizeof(list_t));
-	if (Node == NULL)
-		return (*head);
-	Node->str = strdup(str);
-	if (Node->str == NULL)
+/**
+ * addNode - Add a node
+ * @new_data: pointer to head node
+ * @head_ref: headnode
+ * Return: Head node
+ */
+
+list_t *addNode(list_t **head_ref,
+		const char *new_data)
+{
+	/* Allocate memory for new node*/
+	list_t *new_node;
+	int len = 0;
+	list_t *last;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (new_data == NULL)
 	{
-		free(Node);
-		return (*head);
+		free(new_node);
+		return (NULL);
 	}
-	i = 0;
-	while (str[i])
-		i++;
-	Node->len = i;
-	Node->next = NULL;
-	if (*head == NULL)
-		*head = Node;
+	/* Assign data to new node*/
+	new_node->str = strdup(new_data);
+	len = strlen(new_data);
+	new_node->len = len;
+	new_node->next = NULL;
+	/* Set next pointer of new node to NULL as it will be added at the end*/
+	/* If the linked list is empty, set the new node as the head of the list*/
+	if (*head_ref == NULL)
+	{
+		*head_ref = new_node;
+		return (*head_ref);
+	}
 	else
 	{
-		p = *head;
-		while (p->next != NULL)
-			p = p->next;
-		p->next = Node;
+		/* Traverse the linked list till the end*/
+		last = *head_ref;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
 	}
+	/* Add the new node at the end*/
+	last->next = new_node;
 
-	return (*head);
+	return (*head_ref);
+}
+
+
+/**
+ * add_node_end - return the length of a linked list
+ * @head: head node
+ * @str: string e
+ * Return: Number of nodes
+ */
+
+list_t *add_node_end(list_t **head,
+		const char *str)
+{
+	return (addNode(head, str));
 }
